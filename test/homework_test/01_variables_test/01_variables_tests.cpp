52e6ee47cc54ef75e7daf6b75f618cf7df300092
TomasPen-a <tomas.pena@g.austincc.edu>
2022-02-03 21:30:52 -0600

    HW1 w test cases

--- test/homework_test/01_variables_test/01_variables_tests.cpp
+++ test/homework_test/01_variables_test/01_variables_tests.cpp
@@ -2,13 +2,23 @@
 #include "catch.hpp"
 #include "variables.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
+//TEST_CASE("Verify Test Configuration", "verification") {
+//	REQUIRE(true == true);
+//}
 
-TEST_CASE("Verify sum_numbers function") 
+//TEST_CASE("Verify sum_numbers function") 
+//{
+//	REQUIRE(add_numbers(5, 5) == 10);
+//	REQUIRE(add_numbers(10, 10) == 20);
+//}
+
+TEST_CASE("Verify Test Configuration", "verification") 
 {
-	REQUIRE(add_numbers(5, 5) == 10);
-	REQUIRE(add_numbers(10, 10) == 20);
-}
+	REQUIRE(true == true);
+}                                                                                                    
 
+TEST_CASE("Verify multiply_numbers function", "test multiply")
+{
+	REQUIRE(50 == multiply_numbers(10));
+	REQUIRE(10 == multiply_numbers(2));
+}

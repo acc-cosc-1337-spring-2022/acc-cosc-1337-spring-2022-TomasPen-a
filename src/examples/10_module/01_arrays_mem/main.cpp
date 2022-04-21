b50448330f959309adcc6bf42af4f00321ad6c53
TomasPen-a <tomas.pena@g.austincc.edu>
2022-04-21 16:59:46 -0500

    Class Example - Stack Arrays

--- src/examples/10_module/01_arrays_mem/main.cpp
+++ src/examples/10_module/01_arrays_mem/main.cpp
@@ -1,10 +1,17 @@
-
+#include "arrays_mem.h"
 
 int main() 
 {
+	// use_stack_array();
+	// array_months();
+	// const int SIZE = 3;
+	// int numbers[SIZE]{1,2,3};
+	// display_array(numbers, SIZE);
 
-	
+	int time_table[ROWS][COLS];
+
+	populate_times_table(time_table, ROWS);
+	display_time_table(time_table, ROWS);
 
 	return 0;
-
 }
